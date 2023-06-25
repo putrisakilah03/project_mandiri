86126ab9cf7fb42ca6fb5fe0c010b69e3a639c6f
yudi0576 <129965125+yudi0576@users.noreply.github.com>
2023-06-25 23:39:54 +0700

    Update favorite.dart

--- favorite.dart
+++ favorite.dart
@@ -5,64 +5,61 @@ import 'package:putri_movie/detail_movie.dart';
 class FavoritePages extends StatefulWidget {
   const FavoritePages({super.key});
 
-  @override
-  State<FavoritePages> createState() => _FavoritePagesState();
-}
-
-class _FavoritePagesState extends State<FavoritePages> {
-  List<LastestMovieModels> _listFavoriteMovie = [];
-  @override
+ @override
   void initState() {
     _listFavoriteMovie.addAll([
       LastestMovieModels(
         images: 'assets/film_4.jpg',
-        title: 'END GAME',
+        title: 'Avenger Endgame',
+        genre: 'Genre  : Adventure, Action',
+        durasi: 'Durasi  : 1 jam 59 menit',
+        sutradara: 'Sutradara  : Taka Watiti',
+        rating: 'Rating usia  : 13 tahun',
         actor: 'Tom Hiddleston, Chris Hemsworth, Robert Downey Jr',
         description:
             ' Thor mengisi masa pensiunnya dengan mengembara mencari kedamaian serta berusaha mengembalikan bentuk tubuhnya. Namun, di tengah pengembaraannya Thor kembali diusik dengan kehadiran pembunuh antar galaxi Gorr the God Butcher yang membantai para Dewa.\nGorr the God Butcher memiliki tujuan untuk memusnahkan para Dewa. Menghadapi musuh yang sangat kuat tersebut Thor meminta bantuan King Valkyrie, Korg dan mantan pacarnya Jane Foster.\nBersama-sama, mereka memulai petualangan kosmik yang menegangkan untuk mengungkap dendam dari sang pembantai Dewa dan menghentikannya sebelum terlambat. Namun, yang cukup mengejutkan dalam kisah Thor kali ini, yakni palu ajaib palu ajaibnya, Mjolnir justru merespon kekuatan dan menghampiri Jane di tengah pertempuran.\nKembalinya Natalie Portman sebagai Jane Foster telah lama ditunggu-tunggu para penggemar series hero ini. Pasalnya, Jane tidak muncul lagi sejak putus dengan Thor.',
       ),
       LastestMovieModels(
         images: 'assets/film_5.jpg',
-        title: 'DOCTOR STANGE \nMULTIVERSE OF MADNESS',
+        title: 'DOCTOR STRANGE \MULTIVERSE OF MADNESS',
+        genre: 'Genre  : Adventure, Action',
+        durasi: 'Durasi  : 1 jam 6 menit',
+        sutradara: 'Sutradara  : Sam Raimi',
+        rating: 'Rating usia  : 13',
         actor: 'Scott Rix, Elizabeth Olsen, Hank Pym',
-        description: ' Thor: Love And Thunder (2011)',
-      ),
-      LastestMovieModels(
-        images: 'assets/film_3.jpg',
-        title: 'Avatar 2 The Way of Water',
-        actor: 'Tom Hiddleston, Chris Hemsworth, Robert Downey Jr',
-        description: ' Thor: Love And Thunder (2011)',
-      ),
-      LastestMovieModels(
-        images: 'assets/film_3.jpg',
-        title: 'Avatar 2 The Way of Water',
-        actor: 'Tom Hiddleston, Chris Hemsworth, Robert Downey Jr',
-        description: ' Thor: Love And Thunder (2011)',
-      ),
-      LastestMovieModels(
-        images: 'assets/film_3.jpg',
-        title: 'Avatar 2 The Way of Water',
-        actor: 'Tom Hiddleston, Chris Hemsworth, Robert Downey Jr',
-        description: ' Thor: Love And Thunder (2011)',
-      ),
-      LastestMovieModels(
-        images: 'assets/film_4.jpg',
-        title: 'END GAME',
-        actor: 'Tom Hiddleston, Chris Hemsworth, Robert Downey Jr',
         description:
-            'Thor mengisi masa pensiunnya dengan mengembara mencari kedamaian serta berusaha mengembalikan bentuk tubuhnya. Namun, di tengah pengembaraannya Thor kembali diusik dengan kehadiran pembunuh antar galaxi Gorr the God Butcher yang membantai para Dewa.\nGorr the God Butcher memiliki tujuan untuk memusnahkan para Dewa. Menghadapi musuh yang sangat kuat tersebut Thor meminta bantuan King Valkyrie, Korg dan mantan pacarnya Jane Foster.\nBersama-sama, mereka memulai petualangan kosmik yang menegangkan untuk mengungkap dendam dari sang pembantai Dewa dan menghentikannya sebelum terlambat. Namun, yang cukup mengejutkan dalam kisah Thor kali ini, yakni palu ajaib palu ajaibnya, Mjolnir justru merespon kekuatan dan menghampiri Jane di tengah pertempuran.\nKembalinya Natalie Portman sebagai Jane Foster telah lama ditunggu-tunggu para penggemar series hero ini. Pasalnya, Jane tidak muncul lagi sejak putus dengan Thor.',
-      ),
-      LastestMovieModels(
-        images: 'assets/film_5.jpg',
-        title: 'DOCTOR STANGE \nMULTIVERSE OF MADNESS',
-        actor: 'Scott Rix, Elizabeth Olsen, Hank Pym',
-        description: ' Thor: Love And Thunder (2011)',
+            ' dengan bantuan mstis baik melintasi multiverse yang membahayakan untuk menghadapi musuh',
       ),
       LastestMovieModels(
         images: 'assets/film_3.jpg',
         title: 'Avatar 2 The Way of Water',
-        actor: 'Tom Hiddleston, Chris Hemsworth, Robert Downey Jr',
-        description: ' Thor: Love And Thunder (2011)',
+        genre: 'Genre  : Fiksi ilmiah',
+        durasi: 'Durasi  : 3 jam 12 menit',
+        sutradara: 'Sutradara  : James Cameron',
+        rating: 'Rating usia  : Semua umur',
+        actor: 'Sam Worthington,oe saldana,kate winset',
+        description:
+            ' perjuangan Jake bersama keluarga kecilnya dalam melawan Sky people yg berniat menghancurkan pandora',
+      ),
+      LastestMovieModels(
+        images: 'assets/film_2.jpg',
+        title: 'Ant-Man',
+        genre: 'Genre  : Adventure, Action',
+        durasi: 'Durasi  : 2 jam 4 menit',
+        sutradara: 'Sutradara  : Peter sohn',
+        rating: 'Rating usia  : 17 tahun',
+        actor: '(Paul Rudd, Evageline lilly)',
+        description:'Menjelajahi Alam quantum rempat mereka menemukan mahluk aneh',
+      ),
+      LastestMovieModels(
+        images: 'assets/film_1.jpg',
+        title: 'DOCTOR STANGE \nMULTIVERSE OF MADNESS',
+        genre: 'Genre  : Adventure, Action',
+        durasi: 'Durasi  : 2 jam 6 menit',
+        sutradara: 'Sutradara  : Sam Raimi',
+        rating: 'Rating usia  : 13 tahun',
+        actor: 'Scott Rix, Elizabeth Olsen, Hank Pym',
+        description: ' dengan bantuan mstis baik melintasi multiverse yang membahayakan untuk menghadapi musuh',
       ),
     ]);
     super.initState();
@@ -77,7 +74,7 @@ class _FavoritePagesState extends State<FavoritePages> {
           Container(
             height: 50,
             width: double.infinity,
-            color: Colors.amberAccent,
+            color: Colors.purpleAccent,
             child: const Center(
               child: Text(
                 'Favorite Movie',
@@ -116,6 +113,10 @@ class _FavoritePagesState extends State<FavoritePages> {
                 actor: listMovie[index].actor,
                 description: listMovie[index].description,
                 images: listMovie[index].images,
+                genre: listMovie[index].genre,
+                durasi: listMovie[index].durasi,
+                sutradara: listMovie[index].sutradara,
+                rating: listMovie[index].rating,
               );
             },
           ));
@@ -150,6 +151,34 @@ class _FavoritePagesState extends State<FavoritePages> {
                   Text(
                     listMovie[index].actor,
                     style: const TextStyle(color: Colors.white),
+                    maxLines: 50,
+                    overflow: TextOverflow.ellipsis,
+                    softWrap: false,
+                  ),
+                  Text(
+                    listMovie[index].durasi,
+                    style: const TextStyle(color: Colors.blue),
+                    maxLines: 50,
+                    overflow: TextOverflow.ellipsis,
+                    softWrap: false,
+                  ),
+                  Text(
+                    listMovie[index].genre,
+                    style: const TextStyle(color: Colors.blue),
+                    maxLines: 50,
+                    overflow: TextOverflow.ellipsis,
+                    softWrap: false,
+                  ),
+                  Text(
+                    listMovie[index].sutradara,
+                    style: const TextStyle(color: Colors.blue),
+                    maxLines: 50,
+                    overflow: TextOverflow.ellipsis,
+                    softWrap: false,
+                  ),
+                  Text(
+                    listMovie[index].rating,
+                    style: const TextStyle(color: Colors.blue),
                     maxLines: 50,
                     overflow: TextOverflow.ellipsis,
                     softWrap: false,

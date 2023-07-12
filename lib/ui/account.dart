a4dab9094b4bfd46f6828c43c3d4f124da31078d
putrisakilah03 <129964652+putrisakilah03@users.noreply.github.com>
2023-07-12 18:31:38 +0700

    Update account.dart

--- lib/ui/account.dart
+++ lib/ui/account.dart
@@ -28,7 +28,7 @@ class AccountPages extends StatelessWidget {
             ),
             child: Column(
               children: [
-                _widgetProfile('Nama', 'Putri Syakilah'),
+                _widgetProfile('Nama', 'Putri Sakilah'),
                 _widgetProfile('Npm', '021200103'),
                 _widgetProfile('Handphone', '0895604274943'),
                 _widgetProfile('Alamat', 'Jl.Ratu Sianum'),
